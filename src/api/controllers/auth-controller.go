332597b7f9d1f8d9e99837cf08659a0ca5e61661
jason-gill00 <59940873+jason-gill00@users.noreply.github.com>
2021-05-29 18:56:39 -0400

    Create Account Working

--- src/api/controllers/auth-controller.go
+++ src/api/controllers/auth-controller.go
@@ -1,18 +1,75 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/jason-gill00/coffee-shop-backend-golang/src/api/database"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type CreateUser struct {
+	Username       string `json:"username"`
+	Password       string `json:"password"`
+	VerifyPassword string `json:"verify_password"`
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func Testing(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("INSIDE THE AUTH CONTROLLER")
 }
 
 func CreateAccount(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Inside Create Account Controller")
+	w.Header().Set("Content-Type", "application/json")
+	var createUser CreateUser
+	err := json.NewDecoder(r.Body).Decode(&createUser)
+	if err != nil {
+		panic(err)
+	}
+	log.Print(createUser)
+	if createUser.Password == createUser.VerifyPassword {
+		user, password := createUser.Username, createUser.Password
+		bytePass := []byte(password)
+		hashedPassword, err := bcrypt.GenerateFromPassword(bytePass, bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+		db := database.Connect()
+		database.AddUser(user, string(hashedPassword), db)
+	}
+
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Inside Login Controller")
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		panic(err)
+	}
+	db := database.Connect()
+	database.Login(string(user.Username), string(user.Password), db)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    string(user.Username),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+	_ = godotenv.Load(".env")
+	token, err := claims.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(token)
+	w.Header().Add("auth-token", token)
+
 }
--- src/api/database/dbService.go
+++ src/api/database/dbService.go
@@ -7,8 +7,12 @@ import (
 	"os"
 	"time"
 
+	// "github.com/jason-gill00/coffee-shop-backend-golang/src/api/controllers"
+	// "golang.org/x/crypto/bcrypt"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Order struct {
@@ -20,6 +24,12 @@ type Order struct {
 	Num_milk    int    `json:"num_milk"`
 	Num_cream   int    `json:"num_cream"`
 	Num_sugar   int    `json:"num_sugar"`
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Date     string `json:"date_created"`
 }
 
 func Connect() *sql.DB {
@@ -74,5 +84,35 @@ func CreateOrder(orders []Order, db *sql.DB) {
 		if err != nil {
 			panic(err)
 		}
+	}
+}
+
+func AddUser(username string, password string, db *sql.DB) {
+	datetime := time.Now().Format(time.RFC3339)
+	query := fmt.Sprintf("INSERT INTO users  (username, password, date_created) VALUES ('%s', '%s', '%s');", username, password, datetime)
+	fmt.Println(query)
+	_, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Login(username string, password string, db *sql.DB) {
+	query := fmt.Sprintf("SELECT * FROM users WHERE username LIKE '%s';", username)
+	results, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	var user User
+	for results.Next() {
+		err = results.Scan(&user.Username, &user.Password, &user.Date)
+		if err != nil {
+			log.Fatal("Error parsing row", err)
+		}
+		fmt.Println(user)
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		panic(err)
 	}
 }
